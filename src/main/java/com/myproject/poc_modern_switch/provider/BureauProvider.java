6e192175389cbe5bd58aa604baff99e3e9a5dae6
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-26 22:37:31 -0300

    feat: add provide

---
+++ src/main/java/com/myproject/poc_modern_switch/provider/BureauProvider.java
@@ -0,0 +1,11 @@
+package com.myproject.poc_modern_switch.provider;
+
+public class BureauProvider {
+
+    public int analyze(String cpf) {
+
+        System.out.println("Calling Bureau Provider...");
+
+        return 820;
+    }
+}
