5f054668efeb3f0847794efb5f8a109520f3acd6
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-26 22:48:12 -0300

    feat: add documentatiion

--- README.md
+++ README.md
@@ -23,3 +23,21 @@ fraud-decision-engine
 └── service
     └── FraudAnalysisService.java
 ````
+
+
+````text
+Calling Bureau Provider...
+Calling Device Provider...
+Calling Geo Provider...
+Calling Behavioral Provider...
+Evaluating fraud rules...
+Final Risk Score: 85
+HIGH RISK CUSTOMER
+
+FINAL ANALYSIS
+FraudAnalysis[bureauScore=820, deviceRisk=35, geoRisk=20, behavioralRisk=15]
+
+FINAL DECISION
+REJECTED
+
+````
--- src/main/java/com/myproject/poc_modern_switch/PocModernSwitchApplication.java
+++ src/main/java/com/myproject/poc_modern_switch/PocModernSwitchApplication.java
@@ -1,13 +1,28 @@
 package com.myproject.poc_modern_switch;
 
-import org.springframework.boot.SpringApplication;
+import com.myproject.poc_modern_switch.engine.FraudDecisionEngine;
+import com.myproject.poc_modern_switch.model.FraudAnalysis;
+import com.myproject.poc_modern_switch.service.FraudAnalysisService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PocModernSwitchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocModernSwitchApplication.class, args);
+
+		var fraudAnalysisService = new FraudAnalysisService();
+
+		FraudAnalysis analysis = fraudAnalysisService.analyze("12345678900");
+
+		var engine = new FraudDecisionEngine();
+
+		var decision = engine.evaluate(analysis);
+
+		System.out.println("\nFINAL ANALYSIS");
+		System.out.println(analysis);
+
+		System.out.println("\nFINAL DECISION");
+		System.out.println(decision);
 	}
 
 }
