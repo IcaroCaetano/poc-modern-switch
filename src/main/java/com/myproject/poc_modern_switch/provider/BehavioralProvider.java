8d68b5b792ae62bc0980216a37a742ed0bc5ddeb
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-26 22:39:18 -0300

    feat: add provider

---
+++ src/main/java/com/myproject/poc_modern_switch/provider/BehavioralProvider.java
@@ -0,0 +1,11 @@
+package com.myproject.poc_modern_switch.provider;
+
+public class BehavioralProvider {
+
+    public int analyze(String cpf) {
+
+        System.out.println("Calling Behavioral Provider...");
+
+        return 15;
+    }
+}
