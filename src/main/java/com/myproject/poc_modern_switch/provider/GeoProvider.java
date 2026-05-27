4fb42e4315ea2fc50eb69e1edcf443a9187c9ee9
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-26 22:38:50 -0300

    feat: add provider

---
+++ src/main/java/com/myproject/poc_modern_switch/provider/GeoProvider.java
@@ -0,0 +1,11 @@
+package com.myproject.poc_modern_switch.provider;
+
+public class GeoProvider {
+
+    public int analyze(String cpf) {
+
+        System.out.println("Calling Geo Provider...");
+
+        return 20;
+    }
+}
