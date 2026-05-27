3b861b3b3392e7243aa9752499ba448d0795225f
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-26 22:38:04 -0300

    feat: add provider

---
+++ src/main/java/com/myproject/poc_modern_switch/provider/DeviceProvider.java
@@ -0,0 +1,11 @@
+package com.myproject.poc_modern_switch.provider;
+
+public class DeviceProvider {
+
+    public int analyze(String cpf) {
+
+        System.out.println("Calling Device Provider...");
+
+        return 35;
+    }
+}
